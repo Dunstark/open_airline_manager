419f048289dbcc11cb90aec1d6b63268bc73dc0f
Thomas Seguin <thomas@segu.in>
2016-02-03 20:19:51 +0100

    Added some examples in views.py and urls.py

--- airline_manager/views.py
+++ airline_manager/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'template.html', {})
+
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+
+def user_home(request):
+    return render(request, 'template.html', {})
+
--- open_airline_manager/urls.py
+++ open_airline_manager/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from airline_manager import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
