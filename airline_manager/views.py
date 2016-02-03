d8f7fafd113260a8defe2208169e22a53449600d
Thomas Seguin <thomas@segu.in>
2016-02-03 21:40:23 +0100

    Added login protection to several pages.

--- airline_manager/views.py
+++ airline_manager/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, 'template.html', {})
 
-
+@login_required()
 def profile(request):
     return render(request, 'profile.html', {})
 
-
+@login_required()
 def user_home(request):
     return render(request, 'template.html', {})
 
