d1977bbe9758db4d54822103d2bb714f411d8e0b
Thomas Seguin <thomas@segu.in>
2016-02-20 03:17:34 +0100

    Added some models and started creating tests

--- airline_manager/models.py
+++ airline_manager/models.py
@@ -1,9 +1,78 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import Sum
 from django.contrib.auth.models import User
+
 
 class Airline(models.Model):
     name = models.CharField(max_length=45, unique=True)
     money = models.BigIntegerField(default=100000000)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="airline")
+    alliance = models.ForeignKey("Alliance", null=True, on_delete=models.SET_NULL, related_name="members")
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_founder(self):
+        if self.alliance:
+            return self.alliance.founder == self
+        else:
+            return False
+
+class Alliance(models.Model):
+    name = models.CharField(max_length=45, unique=True)
+    money = models.BigIntegerField(default=0)
+    founder = models.ForeignKey(Airline, on_delete=models.CASCADE, related_name="alliance_founder")
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def member_funds(self):
+        return self.members.aggregate(Sum('money'))['money__sum']
+
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=90, unique=True)
+    city = models.CharField(max_length=90)
+    iata = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.iata + self.city + " - " + self.name
+
+
+class Line(models.Model):
+    start_point = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="start_point")
+    end_point = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="end_point")
+    length = models.IntegerField()
+
+    def __str__(self):
+        return self.start_point + " - " + self.end_point
+
+
+MANUFACTURER_CHOICES = (
+    (0, 'Airbus'),
+    (1, 'Boeing'),
+    (2, 'Bombardier'),
+    (3, 'Embraer'),
+    (4, 'Tupolev'),
+    (5, 'BAE'),
+    (6, 'SAAB'),
+    (7, 'Sukhoi'),
+    (8, 'ATR')
+)
+
+
+class PlaneType(models.Model):
+    name = models.CharField(max_length=20,unique=True)
+    manufacturer = models.IntegerField(choices=MANUFACTURER_CHOICES)
+    range = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.manufacturer + self.name
+
+    def can_fly_line(self, line):
+        return self.range >= line.length
--- airline_manager/tests.py
+++ airline_manager/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from airline_manager.models import Airline, Alliance
 
-# Create your tests here.
+
+class AllianceTestCase(TestCase):
+    def setUp(self):
+        self.a1 = Airline.objects.create(name="Airline 1", owner=User.objects.create(username="o1"), money=2)
+        self.a2 = Airline.objects.create(name="Airline 2", owner=User.objects.create(username="o2"), money=3)
+        self.a3 = Airline.objects.create(name="Airline 3", owner=User.objects.create(username="o3"), money=7)
+        self.alliance1 = Alliance.objects.create(name="TEST Alliance", founder=self.a1, money=11)
+        self.a1.alliance = self.alliance1
+        self.a1.save()
+        self.a2.alliance = self.alliance1
+        self.a2.save()
+
+    def test_correct_owner(self):
+        """Owner is properly identified"""
+        self.assertTrue(self.a1.is_founder)
+        self.assertFalse(self.a2.is_founder)
+        self.assertFalse(self.a3.is_founder)
+
+    def test_member_funds(self):
+        """Alliance members funds is correctly calculated"""
+        self.assertEqual(self.alliance1.member_funds, 5)
