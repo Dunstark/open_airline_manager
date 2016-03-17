09e1430cb7d882557327f0573fd69d1ad87f5c49
Thomas Seguin <thomas@segu.in>
2016-03-17 09:23:41 +0100

    Added a form to modify the setup of a plane

--- airline_manager/forms.py
+++ airline_manager/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from django.core.exceptions import NON_FIELD_ERRORS
-from airline_manager.models import Airline
+from airline_manager.models import Airline, PlaneType, Plane
 
 class AirlineForm(ModelForm):
     class Meta:
         model = Airline
         fields = ['name']
+
+
+class ConfigurationForm(ModelForm):
+    class Meta:
+        model = Plane
+        fields = ['first', 'second', 'third']
